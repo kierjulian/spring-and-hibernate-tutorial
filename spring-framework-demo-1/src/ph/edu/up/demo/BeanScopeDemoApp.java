231f35eef43c4003f00d6b07262c36b911a2a44b
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-05-26 15:44:35 +0800

    Demo scope of beans: prototype and singleton.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

---
+++ spring-framework-demo-1/src/ph/edu/up/demo/BeanScopeDemoApp.java
@@ -0,0 +1,17 @@
+package ph.edu.up.demo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+	
+	public static void main(String[] args) {
+		// Load spring config
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		// Retrieve bean
+		Coach trackCoach = context.getBean("trackCoach", Coach.class);
+		Coach newTrackCoach = context.getBean("trackCoach", Coach.class);
+		System.out.println(trackCoach == (newTrackCoach));
+	}
+
+}
