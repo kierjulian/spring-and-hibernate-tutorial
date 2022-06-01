265798ae4e6a85f8889199187b02f26486701c16
Kier Julian Genoguin <kierj.genoguin@gmail.com>
2022-06-01 14:37:15 +0800

    @RequestMapping demo for controller class.
    Signed-off-by: Kier Julian Genoguin <kierj.genoguin@gmail.com>

--- spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -3,6 +3,6 @@
 	<body>
 		<h2>Spring MVC Demo - Home Page</h2>
 		<hr>
-		<a href="showForm">Hello world form</a>
+		<a href="hello/showForm">Hello world form</a>
 	</body>
 </html>
---
+++ spring-mvc-demo/src/ph/edu/up/mvc/SillyController.java
@@ -0,0 +1,15 @@
+package ph.edu.up.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class SillyController {
+	
+	@RequestMapping("/showForm")
+	public String displayTheForm() {
+		return "silly";
+	}
+
+}
